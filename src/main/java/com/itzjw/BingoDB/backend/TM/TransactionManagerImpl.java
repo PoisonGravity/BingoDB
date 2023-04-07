9584a4886024e9086e5436149e7f9544147f99f0
PoisonGravity <107403926+PoisonGravity@users.noreply.github.com>
2023-04-07 22:20:14 +0100

    initialization TransactionManager InterfaceImpl Statics

--- src/main/java/com/itzjw/BingoDB/backend/TM/TransactionManagerImpl.java
+++ src/main/java/com/itzjw/BingoDB/backend/TM/TransactionManagerImpl.java
@@ -1,7 +1,22 @@
 package com.itzjw.BingoDB.backend.TM;
 
-public class TransactionManagerImpl implements TransactionManager{
+public class TransactionManagerImpl implements TransactionManager {
 
+    static final int LEN_XID_HEAD_LENGTH = 8;   //XID头文件长度
+
+    private static final int XID_FIELD_SIZE = 1; //每个事务的的占用长度
+
+
+    //事务的三种状态
+    private static final byte FIELD_TRAN_ACTIVE = 0;
+    private static final byte FIELD_TRAN_COMMITTED = 1;
+    private static final byte FIELD_TRAN_ABORTED = 2;
+
+    //超级事务，永远为COMMITED状态
+    public static final long SUPER_XID = 0;
+
+    //XID文件后缀
+    static final String XID_SUFFIX = ".xid";
 
     public long begin() {
         return 0;
