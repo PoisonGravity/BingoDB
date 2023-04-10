1a91259cd339e68aa808c9fa0d54324cf90740b6
PoisonGravity <107403926+PoisonGravity@users.noreply.github.com>
2023-04-10 23:54:45 +0100

    initialization Parser parseLong

--- src/main/java/com/itzjw/BingoDB/backend/utils/Parser.java
+++ src/main/java/com/itzjw/BingoDB/backend/utils/Parser.java
@@ -1,6 +1,11 @@
 package com.itzjw.BingoDB.backend.utils;
 
+import java.nio.ByteBuffer;
+
 public class Parser {
 
-
+    public static long parseLong(byte[] buf){
+        ByteBuffer buffer = ByteBuffer.wrap(buf,0,8);
+        return buffer.getLong();
+    }
 }
