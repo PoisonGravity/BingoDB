d11eca9788933f7326fe1b2f86e51965429c9fb2
PoisonGravity <107403926+PoisonGravity@users.noreply.github.com>
2023-04-14 00:20:28 +0100

    initialization PageCache

--- src/main/java/com/itzjw/BingoDB/backend/Dm/pageCache/PageCache.java
+++ src/main/java/com/itzjw/BingoDB/backend/Dm/pageCache/PageCache.java
@@ -1,5 +1,24 @@
 package com.itzjw.BingoDB.backend.Dm.pageCache;
 
-public class PageCache {
+import com.itzjw.BingoDB.backend.Dm.page.Page;
+
+public interface PageCache {
     public static final int PAGE_SIZE = 1 << 13;
+
+    int newPage(byte[] initData);
+
+    Page getPage(int pgno) throws Exception;
+
+    void close();
+
+    void release(Page page);
+
+    void truncateByPgno(int maxPgno);
+
+    int getPageNumber();
+
+    void flushPage(Page pg);
+
+
+
 }
